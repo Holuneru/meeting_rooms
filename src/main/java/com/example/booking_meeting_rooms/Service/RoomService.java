dfd7a8d8d1cfc182e34393e4e5b326f5420433c7
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-04-08 17:15:40 +0300

    CreateRoom Service

---
+++ src/main/java/com/example/booking_meeting_rooms/Service/RoomService.java
@@ -0,0 +1,12 @@
+package com.example.booking_meeting_rooms.Service;
+
+import com.example.booking_meeting_rooms.Entity.Room;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RoomService {
+    private final RoomService roomService;
+
+}
