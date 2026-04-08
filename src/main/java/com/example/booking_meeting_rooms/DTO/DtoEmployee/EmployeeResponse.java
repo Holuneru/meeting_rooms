778e3b326b9f049e9465ca35293a807187bd5d74
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-04-08 17:05:54 +0300

    Add CreateEmployee function

--- src/main/java/com/example/booking_meeting_rooms/DTO/DtoEmployee/EmployeeResponse.java
+++ src/main/java/com/example/booking_meeting_rooms/DTO/DtoEmployee/EmployeeResponse.java
@@ -2,10 +2,6 @@ package com.example.booking_meeting_rooms.DTO.DtoEmployee;
 
 import com.example.booking_meeting_rooms.Entity.BookingFolder.Booking;
 import com.example.booking_meeting_rooms.Entity.EmployeesFolder.EmployeeRole;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.List;
